e856e5ad78abf2012837862b791e492220351811
fraolb <Fraolbereket@gmail.com>
2025-04-16 09:37:49 +0300

    main page ui

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -10,6 +10,9 @@ import {
   ArrowUpCircleIcon,
   ArrowsUpDownIcon,
   ArrowDownIcon,
+  ArrowDownCircleIcon,
+  CurrencyDollarIcon,
+  CurrencyEuroIcon,
 } from "@heroicons/react/24/solid";
 
 import { Card } from "@/components/ui/card";
@@ -62,6 +65,67 @@ export default function Home() {
             </Button>
           </div>
         </Card>
+        <div className="w-full">
+          <div className="flex justify-between px-2 mb-2">
+            <h3 className="font-bold font-mono">Latest Transactions</h3>
+            <div className="font-light">See all</div>
+          </div>
+          <Card className="w-full bg-gray-50 flex flex-row justify-beteween px-2 py-2 mb-2">
+            <div className="text-red-700 pt-1">
+              <ArrowUpCircleIcon className="w-10 h-10" />
+            </div>
+
+            <div className="block w-3/5">
+              <div>To 10 blocks of Ice</div>
+              <div className="text-sm">0xabcd32r342...</div>
+            </div>
+            <span className="w-1/5 text-red-700 pt-2 text-center">- 250 $</span>
+          </Card>
+          <Card className="w-full bg-gray-50 flex flex-row justify-beteween px-2 py-2 mb-2">
+            <div className="text-green-700 pt-1">
+              <ArrowDownCircleIcon className="w-10 h-10" />
+            </div>
+
+            <div className="block w-3/5">
+              <div>Buy Macbook Pc</div>
+              <div className="text-sm">0xabcd32r342...</div>
+            </div>
+            <span className="w-1/5 text-green-700 pt-2 text-center">
+              + 750 $
+            </span>
+          </Card>
+        </div>
+        <div className="w-full">
+          <div className="flex justify-left px-2 mb-2">
+            <h3 className="font-bold font-mono">Currency</h3>
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+            <Card className="px-2 py-2 mb-2 gap-0 bg-gray-50">
+              <div className="text-green-700 pt-1">
+                <CurrencyDollarIcon className="w-10 h-10" />
+              </div>
+
+              <div className="block">cUSD</div>
+              <span className="text-green-700">250 $</span>
+            </Card>
+            <Card className="px-2 py-2 mb-2 gap-0 bg-gray-50">
+              <div className="text-blue-700 pt-1">
+                <CurrencyEuroIcon className="w-10 h-10" />
+              </div>
+
+              <div className="block">cEur</div>
+              <span className="text-green-700">120 $</span>
+            </Card>
+            <Card className="px-2 py-2 mb-2 gap-0 bg-gray-50">
+              <div className="text-yellow-400 pt-1">
+                <CurrencyDollarIcon className="w-10 h-10" />
+              </div>
+
+              <div className="block">Celo</div>
+              <span className="text-green-700">50 $</span>
+            </Card>
+          </div>
+        </div>
       </div>
     </div>
   );
