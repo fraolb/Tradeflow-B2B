b7e8001d1ba9b6a13fd731ea9bd291ad81dd41f5
fraolb <Fraolbereket@gmail.com>
2025-04-17 21:51:04 +0300

    contract functions

---
+++ front-end/lib/ContractFunctions.ts
@@ -0,0 +1,73 @@
+// lib/contractFunctions.ts
+"use client";
+
+import { TxData } from "@/types/transaction";
+import { config } from "@/app/Provider";
+import {
+  readContract,
+  writeContract,
+  waitForTransactionReceipt,
+} from "@wagmi/core";
+import TradeflowB2B from "@/ABI/TradeflowB2B.json";
+
+const CONTRACT_ADDRESS = "0x92c7d8B28b2c487c7f455733470B27ABE2FefF13";
+
+export const getNameFromContract = async (address: string): Promise<string> => {
+  const result = await readContract(config, {
+    address: CONTRACT_ADDRESS as `0x${string}`,
+    abi: TradeflowB2B,
+    functionName: "getUserName",
+    args: [address],
+  });
+
+  return result as string;
+};
+
+export const getTxsFromContract = async (
+  address: string
+): Promise<TxData[]> => {
+  const result = await readContract(config, {
+    address: CONTRACT_ADDRESS as `0x${string}`,
+    abi: TradeflowB2B,
+    functionName: "getUserTransactions",
+    args: [address],
+  });
+
+  // Ensure the contract returns an array of structs
+  return (result as any[]).map(
+    (tx: any): TxData => ({
+      counterparty: tx.counterparty,
+      stablecoin: tx.stablecoin,
+      amount: Number(tx.amount),
+      timestamp: Number(tx.timestamp),
+      blockNumber: Number(tx.blockNumber),
+      reason: tx.reason,
+      txType: Number(tx.txType),
+    })
+  );
+};
+
+export const payUser = async ({
+  stablecoin,
+  receiver,
+  amount,
+  reason,
+}: {
+  stablecoin: `0x${string}`;
+  receiver: `0x${string}`;
+  amount: bigint;
+  reason: string;
+}) => {
+  const tx = await writeContract(config, {
+    address: CONTRACT_ADDRESS as `0x${string}`,
+    abi: TradeflowB2B,
+    functionName: "pay",
+    args: [stablecoin, receiver, amount, reason],
+  });
+
+  const receipt = await waitForTransactionReceipt(config, {
+    hash: tx,
+  });
+
+  return receipt;
+};
